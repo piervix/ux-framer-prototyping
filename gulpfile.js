ebc65bf2f89bdbff95a80236e5224caafe75ecb2
Piervincenzo Madeo <pier.made@gmail.com>
2016-03-06 19:18:24 +0100

    Deleted dummy log

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,6 @@ var webpackCfg = require('./webpack.config.js');
 
 gulp.task('webpack', function (callback) {
   webpack(webpackCfg, function (err, stats) {
-    console.log('Callback: ' + callback);
     if (err) throw new gutil.PluginError('webpack', err);
     gutil.log('[webpack]', stats.toString({
         // TODO: add config
