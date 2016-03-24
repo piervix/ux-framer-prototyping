d21841c2629749b60ce85ea4f6ae4f22a26b1f11
Piervincenzo Madeo <pier.made@gmail.com>
2016-03-24 11:45:26 +0100

    A better gulpfile for a better workflow

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,10 @@ var coffee = require('gulp-coffee');
 var gutil = require('gulp-util');
 var watch = require('gulp-watch');
 var sketch = require('gulp-sketch');
-var clean = require('gulp-clean');
-var runIf = require('gulp-if');
+var debug = require('gulp-debug');
+var del = require('del');
 var webpack = require('webpack');
+var mergeStream = require('merge-stream');
 var browserSync = require('browser-sync');
 var path = require('path');
 var argv = require('yargs').argv;
@@ -15,41 +16,67 @@ var argv = require('yargs').argv;
 var webpackCfg = require('./webpack.config.js');
 
 var paths = {
-  framerModuleBuilded: path.join(__dirname, 'node_modules/framerjs/build/*'),
+  framerModuleBuild: path.join(__dirname, 'node_modules/framerjs/build/*'),
   build: path.join(__dirname, 'build'),
   buildFramer: path.join(__dirname, 'build/framer'),
   buildFolders: path.join(__dirname, 'build/**/*.*'),
-  buildFiles: path.join(__dirname, 'build/*.*'),
   buildImages: path.join(__dirname, 'build/images'),
-  buildImported: path.join(__dirname, 'build/imported'),
   src: path.join(__dirname, 'src'),
   appIndex: path.join(__dirname, 'src/app.coffee'),
   appHTML: path.join(__dirname, 'src/index.html'),
   appModules: path.join(__dirname, 'src/**/*.coffee'),
   importedFromSketchFold: path.join(__dirname, 'src/**/imported/**/*'),
-  importedFromSketch: path.join(__dirname, 'src/**/imported/**/*.json'),
+  importedFromSketchJson: path.join(__dirname, 'src/**/imported/**/*.json'),
   sketchSlices: path.join(__dirname, 'src/*.slices.sketch'),
   appImages: path.join(__dirname, 'src/images/**/*.{png, jpg, svg}'),
   appCSS: path.join(__dirname, 'src/style.css'),
 };
 
-gulp.task('clean', function () {
-  return gulp.src(paths.build, { read: false })
-    .pipe(clean());
-});
+var sketchSlices = argv.slices || false;
+var copyTask = (sketchSlices) ? 'sketch:slices' : 'copy:generator';
+var sketchWatchPath = (sketchSlices) ? paths.sketchSlices : paths.importedFromSketchJson;
+
+gulp.task('clean', del.bind(null, paths.build));
 
 gulp.task('webpack', function (callback) {
   webpack(webpackCfg, function (err, stats) {
     if (err) throw new gutil.PluginError('webpack', err);
     gutil.log('[webpack]', stats.toString({
-        // TODO: add config
+      colors: true,
     }));
     callback();
   });
 });
 
-gulp.task('sketch:slices', function () {
-  gulp.src(paths.sketchSlices)
+gulp.task('copy:framerjs', function () {
+  var stream = gulp.src(paths.framerModuleBuild)
+    .pipe(gulp.dest(paths.buildFramer));
+
+  return stream;
+});
+
+gulp.task('copy:static', function () {
+  var htmlStream = gulp.src(paths.appHTML)
+    .pipe(gulp.dest(paths.build));
+
+  var imagesStream = gulp.src(paths.appImages)
+    .pipe(gulp.dest(paths.buildImages));
+
+  var cssStream = gulp.src(paths.appCSS)
+    .pipe(gulp.dest(paths.build));
+
+  return mergeStream(htmlStream, imagesStream, cssStream);
+});
+
+gulp.task('copy:generator', ['copy:static', 'copy:framerjs'], function () {
+  var stream = gulp.src(paths.importedFromSketchFold)
+    .pipe(gulp.dest(paths.build));
+
+  return stream;
+});
+
+gulp.task('sketch:slices', ['copy:static', 'copy:framerjs'], function () {
+  var stream = gulp.src(paths.sketchSlices)
     .pipe(sketch({
       export: 'slices',
       format: 'png',
@@ -58,36 +85,13 @@ gulp.task('sketch:slices', function () {
       trimmed: false,
     }))
     .pipe(gulp.dest(paths.buildImages));
+
+  return stream;
 });
 
-gulp.task('copy', function () {
-  gulp.src(paths.framerModuleBuilded)
-    .pipe(gulp.dest(paths.buildFramer));
-  gulp.src(paths.appHTML)
-    .pipe(gulp.dest(paths.build));
-  gulp.src(paths.appImages)
-    .pipe(gulp.dest(paths.buildImages));
-  gulp.src(paths.appCSS)
-    .pipe(gulp.dest(paths.build));
-  gulp.src(paths.importedFromSketchFold)
-    .pipe(gulp.dest(paths.build));
-});
-
-gulp.task('copy:imported', function () {
-  gulp.src(paths.buildImported, { read: false })
-    .pipe(clean());
-  gulp.src(paths.importedFromSketchFold)
-    .pipe(gulp.dest(paths.build));
-});
-
-gulp.task('watch', function () {
+gulp.task('watch:serve', function () {
   gulp.watch([paths.appIndex, paths.appModules], ['webpack']);
-  if (argv.slices) {
-    console.log('slices!');
-    gulp.watch(paths.sketchSlices, ['sketch:slices']);
-  } else {
-    gulp.watch(paths.importedFromSketch, ['copy:imported']);
-  }
+  gulp.watch(sketchWatchPath, [copyTask]);
 
   browserSync({
     server: {
@@ -95,10 +99,12 @@ gulp.task('watch', function () {
     },
     browser: 'google chrome',
     injectChanges: false,
-    files: [paths.buildFolders, paths.buildFolders],
+    files: [paths.buildFolders],
     notify: true,
   });
 });
 
-gulp.task('build', ['clean', 'copy', 'webpack']);
-gulp.task('default', ['build', 'watch'], browserSync.reload);
+gulp.task('build', [copyTask, 'webpack']);
+gulp.task('default', ['clean'], function () {
+  gulp.start(['build', 'watch:serve']);
+});
